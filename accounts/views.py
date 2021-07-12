1f7e9ddcded01eea2090d7e8789472d4a110267a
hizashi <zytwp@hotmail.com>
2021-07-13 00:31:50 +0800

    third commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
+from accounts.forms import RegisterUserForm
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 def register_view(request):
-    pass
+    register = RegisterUserForm()
+    if request.method == "POST":
+        user = register.save()
+        login(request, user)
+    
+    context = {"register_form" : register}
+
+    return render(request, "accounts/register.html", context)
 
 def login_view(request):
     pass
